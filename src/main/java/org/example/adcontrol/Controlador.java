453a4133003c66deda619952357dba16d88325e0
Bertitox <albercg18@gmail.com>
2025-03-21 11:03:20 +0100

    Nueva clase controlesMenu.fxml

--- src/main/java/org/example/adcontrol/Controlador.java
+++ src/main/java/org/example/adcontrol/Controlador.java
@@ -1,81 +1,14 @@
 package org.example.adcontrol;
 
 import javafx.animation.ScaleTransition;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
-import java.io.IOException;
 
 public class Controlador {
-    @FXML
-    private Pane panelPrincipal;
-
-    /**
-     * Método que sirve para cambiar la pantalla de Configuracion
-     *
-     * @param event Evento que inicia el método
-     * @throws IOException Excepción de entrada salida lanzada por el método
-     */
-    @FXML
-    void cambiarpantallaConfig(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaInforme.fxml"));
-        Parent root = fxmlLoader.load();
-        panelPrincipal.getChildren().clear();
-        panelPrincipal.getChildren().add(root);
-    }
-
-    /**
-     * Método que se encarga de cambiar la pantalla actual por la correspondiente al botón pulsado (pantalla de administradorS de equipos)
-     *
-     * @param event evento que espera el método
-     * @throws IOException excepción de entrada salida lanzada por el método
-     */
-    @FXML
-    void cambiarPantallaMonitor(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaMonitor.fxml"));
-        Parent root = fxmlLoader.load();
-        panelPrincipal.getChildren().clear();
-        panelPrincipal.getChildren().add(root);
-    }
-
-    /**
-     * Método que sirve para cambiar la pantalla Home
-     *
-     * @param event Evento que inicia el método
-     * @throws IOException Excepción de entrada salida lanzada por el método
-     */
-    @FXML
-    void cambiarPantallaHome(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaHome.fxml"));
-        Parent root = fxmlLoader.load();
-        panelPrincipal.getChildren().clear();
-        panelPrincipal.getChildren().add(root);
-    }
-
-    /**
-     * Método que sirve para cambiar la pantalla de Ayuda
-     *
-     * @param event Evento que inicia el método
-     * @throws IOException Excepción de entrada salida lanzada por el método
-     */
-    @FXML
-    void cambiarpantallaAyuda(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaAyuda.fxml"));
-        Parent root = fxmlLoader.load();
-        panelPrincipal.getChildren().clear();
-        panelPrincipal.getChildren().add(root);
-    }
-
     /**
      * Método que añade un efecto de zoom cuando se superpone el cursor por encima de un boton
      *
@@ -136,20 +69,4 @@ public class Controlador {
         zoomIn.play();
     }
 
-    /**
-     * Evento que muestra al usuario una ventana de confirmación par salir o no de la app
-     *
-     * @param event Evento de pulsacion del botón
-     */
-    public void salir(Event event) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Estás seguro de que desea salir?", ButtonType.YES, ButtonType.NO);
-        alert.setTitle("Salir de ADControl");
-        alert.setHeaderText(null); //Elimina el encabezado
-
-        //Mostrar diálogo y esperar a la respuesta del usuario
-        if (alert.showAndWait().get() == ButtonType.YES) {
-            Platform.exit();
-            System.exit(0);
-        }
-    }
 }
---
+++ src/main/java/org/example/adcontrol/ControladorMenu.java
@@ -0,0 +1,105 @@
+package org.example.adcontrol;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class ControladorMenu extends Controlador {
+    @FXML
+    public Pane panelPrincipal;
+
+    @FXML
+    void initialize() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaHome.fxml"));
+            Parent root = fxmlLoader.load();
+            panelPrincipal.getChildren().clear();
+            panelPrincipal.getChildren().add(root);
+
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+
+    /**
+     * Método que sirve para cambiar la pantalla de Configuracion
+     *
+     * @param event Evento que inicia el método
+     * @throws IOException Excepción de entrada salida lanzada por el método
+     */
+    @FXML
+    void cambiarpantallaInforme(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaInforme.fxml"));
+        Parent root = fxmlLoader.load();
+        panelPrincipal.getChildren().clear();
+        panelPrincipal.getChildren().add(root);
+    }
+
+    /**
+     * Método que se encarga de cambiar la pantalla actual por la correspondiente al botón pulsado (pantalla de administradorS de equipos)
+     *
+     * @param event evento que espera el método
+     * @throws IOException excepción de entrada salida lanzada por el método
+     */
+    @FXML
+    void cambiarPantallaMonitor(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaMonitor.fxml"));
+        Parent root = fxmlLoader.load();
+        panelPrincipal.getChildren().clear();
+        panelPrincipal.getChildren().add(root);
+    }
+
+    /**
+     * Método que sirve para cambiar la pantalla Home
+     *
+     * @param event Evento que inicia el método
+     * @throws IOException Excepción de entrada salida lanzada por el método
+     */
+    @FXML
+    void cambiarPantallaHome(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaHome.fxml"));
+        Parent root = fxmlLoader.load();
+        panelPrincipal.getChildren().clear();
+        panelPrincipal.getChildren().add(root);
+    }
+
+
+    /**
+     * Método que sirve para cambiar la pantalla de Ayuda
+     *
+     * @param event Evento que inicia el método
+     * @throws IOException Excepción de entrada salida lanzada por el método
+     */
+    @FXML
+    void cambiarpantallaAyuda(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/vistaAyuda.fxml"));
+        Parent root = fxmlLoader.load();
+        panelPrincipal.getChildren().clear();
+        panelPrincipal.getChildren().add(root);
+    }
+
+    /**
+     * Evento que muestra al usuario una ventana de confirmación par salir o no de la app
+     *
+     * @param event Evento de pulsacion del botón
+     */
+    public void salir(Event event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Estás seguro de que desea salir?", ButtonType.YES, ButtonType.NO);
+        alert.setTitle("Salir de ADControl");
+        alert.setHeaderText(null); //Elimina el encabezado
+
+        //Mostrar diálogo y esperar a la respuesta del usuario
+        if (alert.showAndWait().get() == ButtonType.YES) {
+            Platform.exit();
+            System.exit(0);
+        }
+    }
+}
