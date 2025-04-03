1ab6a90c1ff1b8ec8ab635e5d9a3f38939dc14e9
Bertitox <albercg18@gmail.com>
2025-04-03 14:31:40 +0200

    Mapa acabado, corregir colores

--- src/main/java/org/example/adcontrol/ControladorMapa.java
+++ src/main/java/org/example/adcontrol/ControladorMapa.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 
 import javax.swing.*;
 
@@ -16,11 +17,43 @@ public class ControladorMapa {
     private Button DAM1;
 
     @FXML
+    private Rectangle rectSecret;
+
+    @FXML
+    private Rectangle rectaulainf;
+
+    @FXML
+    private Rectangle rectprof;
+
+    @FXML
     void cambiarAula(ActionEvent event) {
         Button b = (Button) event.getSource();
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + b.getText());
         alert.setTitle("Cambiando de aula");
         alert.setHeaderText(null); //Elimina el encabezado
         alert.showAndWait();
+    }
+
+    @FXML
+    void botonMapaHover(MouseEvent event) {
+        Button b = (Button) event.getSource();
+        if(b.getText().equals("Aula de informatica 1")) {
+            rectaulainf.setOpacity(0.5);
+        }else if(b.getText().equals("Secretaria")) {
+            rectSecret.setOpacity(0.5);
+        }else{
+            rectprof.setOpacity(0.5);
+        }
+        b.setOpacity(0.5);
+    }
+
+    @FXML
+    void botonMapaNormal(MouseEvent event) {
+        Button b = (Button) event.getSource();
+        b.setOpacity(1);
+        rectaulainf.setOpacity(1);
+        rectSecret.setOpacity(1);
+        rectprof.setOpacity(1);
+
     }
 }
