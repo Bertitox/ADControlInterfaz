4cb329c0715802ee8ed492e485f97f7051eedef4
Daniel García <Dagarlow@gmail.com>
2025-04-03 12:35:56 +0200

    Mejoras en la versión de la vistaAula.fxml y ControladorAula.java. También he realizado nuevas mejoras en CRUDAula.java y algunos métodos relevantes para las clases mencionadas.

--- src/main/java/BBDD/DAO/CRUDAula.java
+++ src/main/java/BBDD/DAO/CRUDAula.java
@@ -2,13 +2,15 @@ package BBDD.DAO;
 
 import BBDD.Conexion.Conexion;
 import BBDD.DTO.Aula;
-import BBDD.DTO.InformacionSistema;
 import BBDD.Excepciones.AulaNotFoundException;
 import BBDD.Validaciones.Validaciones_systema;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Case que realiza las operaciónes básicas para la Entidad Aula
@@ -37,7 +39,7 @@ public class CRUDAula {
      * @param aula Objeto {@link Aula}
      */
     public void insertAula(Aula aula) {
-        if (!(validaciones.onlyOneDevice(aula.getIdInformacionSistema().getId())) && (!(validaciones.existeAula(aula.getReferencia())))) {
+        if (!(validaciones.onlyOneDevice(aula.getIdInformacionSistema().getId()))) {
             transaction.begin();
             gestorEntidad.persist(aula);
             transaction.commit();
@@ -98,6 +100,77 @@ public class CRUDAula {
     public void mostrarAulas(){
         for (Aula a : readAllAulas()) {
             System.out.println(a);
+        }
+    }
+
+    /**
+     * Método que devuelve el número de equipos por aula.
+     * @param referencia referencia del aula a consultar.
+     * @return devuelve un entero con el número de equipos por aula.
+     */
+    public String numEquiposXAula(String referencia) {
+        try {
+            //Consulta que devuelve el num de equipos por aula
+            Object result = gestorEntidad.createNativeQuery("""
+            SELECT COUNT(id_informacion_sistema) 
+            FROM aula 
+            WHERE referencia = :referencia
+            GROUP BY referencia
+            """)
+                    .setParameter("referencia", referencia)  //Usamos setParameter para inyectar la variable
+                    .getSingleResult();  //Devolvemos el resultado de la consulta
+
+            //Convertimos el resultado en un entero
+            int resultado = (result != null) ? ((Number) result).intValue() : 0;
+            return String.valueOf(resultado);
+
+        //Gestionamos el resultado en el caso de que sea 0 o un num negativo
+        } catch (jakarta.persistence.NoResultException e) {
+            return "0";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "-1";
+        }
+    }
+
+    /**
+     * Método que devuelve un mapa con cada aula como clave y el número de equipos por aula de valor.
+     * @return devuelve un mapa con cada aula como clave y el número de equipos por aula de valor.
+     */
+    public Map<String, Integer> mapEquiposPorAula() {
+        Map<String, Integer> equiposPorAula = new LinkedHashMap<>(); //Hacemos el mapa LinkedHashMap para que se ordene de menor a mayor en orden natural
+
+        try {
+            //Ejecutamos la consulta
+            List<Object[]> resultados = gestorEntidad.createNativeQuery("""
+            SELECT referencia AS aula, COUNT(id_informacion_sistema) AS total_equipos
+            FROM aula
+            GROUP BY referencia
+            """).getResultList();
+
+            //Agregamos los resultados de la consulta al mapa
+            for (Object[] row : resultados) {
+                String referencia = (String) row[0]; //Agregamos la referencia (clave)
+                Integer totalEquipos = ((Number) row[1]).intValue(); //Agregamos el conteo de los equipos (valor)
+                equiposPorAula.put(referencia, totalEquipos);  //Agregamos los valores al mapa
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return equiposPorAula;  //Devolvemos el mapa
+    }
+
+
+    public static void main(String[] args) {
+        CRUDAula c = new CRUDAula();
+        //c.insertAula(new Aula("PRUEBA6", c.gestorEntidad.find(InformacionSistema.class, 7)));
+        //System.out.println(c.numEquiposXAula("PRUEBA5"));
+
+        for (Map.Entry<String, Integer> entry : c.mapEquiposPorAula().entrySet()) {
+            String referencia = entry.getKey();
+            Integer totalEquipos = entry.getValue();
+            System.out.println("Aula: " + referencia + " - Total de equipos: " + totalEquipos);
         }
     }
 }
