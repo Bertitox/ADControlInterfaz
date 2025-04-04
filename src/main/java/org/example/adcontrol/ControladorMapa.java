e30b33f582dc2c207a758fbe27a71e05b0ecd3e3
Bertitox <albercg18@gmail.com>
2025-04-04 09:44:29 +0200

    Mapa acabado finalmente

--- src/main/java/org/example/adcontrol/ControladorMapa.java
+++ src/main/java/org/example/adcontrol/ControladorMapa.java
@@ -13,15 +13,6 @@ import javax.swing.*;
 
 public class ControladorMapa {
     @FXML
-    private Rectangle rectSecret;
-
-    @FXML
-    private Rectangle rectaulainf;
-
-    @FXML
-    private Rectangle rectprof;
-
-    @FXML
     private Button DespachoProfesores2;
 
     @FXML
@@ -31,82 +22,55 @@ public class ControladorMapa {
     private Button secretaria;
 
     @FXML
+    private Button DespachoProfesores2rect;
+
+    @FXML
+    private Button secretariarect;
+
+    @FXML
+    private Button aulaInfo1rect;
+
+
+    @FXML
     void cambiarAula(ActionEvent event) {
-        Alert alert = null;
-        if (event.getSource() == Rectangle.class) {
-            Rectangle rectangle = (Rectangle) event.getSource();
-            if (rectangle.getId().equalsIgnoreCase("rectSecret")) {
-                alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + secretaria.getText());
-            } else if (rectangle.getId().equalsIgnoreCase("rectprof")) {
-                alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + DespachoProfesores2.getText());
-            } else if (rectangle.getId().equalsIgnoreCase("rectaulainf")) {
-                alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + aulaInfo1.getText());
-            }
-        } else {
-            Button b = (Button) event.getSource();
-            alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + b.getText());
-        }
+        Button b = (Button) event.getSource();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Cambiando a " + b.getText());
         alert.setTitle("Cambiando de aula");
-        alert.setHeaderText(null); //Elimina el encabezado
+        alert.setHeaderText(null);
         alert.showAndWait();
     }
 
     @FXML
     void botonMapaHover(MouseEvent event) {
-        if (event.getSource().getClass() == Button.class) {
-            Button b = (Button) event.getSource();
-            if (b.getText().equalsIgnoreCase("Aula de informatica 1")) {
-                rectaulainf.setOpacity(0.5);
-            } else if (b.getText().equalsIgnoreCase("Secretaria")) {
-                rectSecret.setOpacity(0.5);
-            } else if (b.getText().equalsIgnoreCase("Desp. Prof 2")) {
-                rectprof.setOpacity(0.5);
-            }
+        Button b = (Button) event.getSource();
+        if (b.getId().equalsIgnoreCase("aulaInfo1rect") || b.getId().equalsIgnoreCase("aulaInfo1")) {
+            aulaInfo1.setOpacity(0.5);
+            aulaInfo1rect.setOpacity(0.5);
+        } else if (b.getId().equalsIgnoreCase("secretariarect") || b.getId().equalsIgnoreCase("secretaria")) {
+            secretariarect.setOpacity(0.5);
+            secretaria.setOpacity(0.5);
+        } else if (b.getId().equalsIgnoreCase("DespachoProfesores2rect") || b.getId().equalsIgnoreCase("DespachoProfesores2")) {
+            DespachoProfesores2rect.setOpacity(0.5);
+            DespachoProfesores2.setOpacity(0.5);
+        } else {
             b.setOpacity(0.5);
-        }
-
-        if (event.getSource().getClass() == Rectangle.class) {
-            Rectangle r = (Rectangle) event.getSource();
-            if (r.getId().equalsIgnoreCase("rectaulainf")) {
-                aulaInfo1.setOpacity(0.5);
-            } else if (r.getId().equalsIgnoreCase("rectSecret")) {
-                secretaria.setOpacity(0.5);
-            } else if (r.getId().equalsIgnoreCase("rectprof")) {
-                DespachoProfesores2.setOpacity(0.5);
-            }
-            r.setOpacity(0.5);
         }
     }
 
     @FXML
     void botonMapaNormal(MouseEvent event) {
-        if (event.getSource().getClass() == Button.class) {
-            Button b = (Button) event.getSource();
-            if (b.getText().equals("Aula de informatica 1")) {
-                rectaulainf.setOpacity(1);
-            } else if (b.getText().equals("Secretaria")) {
-                rectSecret.setOpacity(1);
-            } else if (b.getText().equals("Desp. Prof 2")) {
-                rectprof.setOpacity(1);
-            } else {
-
-            }
+        Button b = (Button) event.getSource();
+        if (b.getId().equalsIgnoreCase("aulaInfo1rect") || b.getId().equalsIgnoreCase("aulaInfo1")) {
+            aulaInfo1.setOpacity(1);
+            aulaInfo1rect.setOpacity(1);
+        } else if (b.getId().equalsIgnoreCase("secretariarect") || b.getId().equalsIgnoreCase("secretaria")) {
+            secretariarect.setOpacity(1);
+            secretaria.setOpacity(1);
+        } else if (b.getId().equalsIgnoreCase("DespachoProfesores2rect") || b.getId().equalsIgnoreCase("DespachoProfesores2")) {
+            DespachoProfesores2rect.setOpacity(1);
+            DespachoProfesores2.setOpacity(1);
+        } else {
             b.setOpacity(1);
         }
-
-        if (event.getSource().getClass() == Rectangle.class) {
-            Rectangle r = (Rectangle) event.getSource();
-            if (r.getId().equals("rectaulainf")) {
-                aulaInfo1.setOpacity(1);
-            } else if (r.getId().equals("rectSecret")) {
-                secretaria.setOpacity(1);
-            } else if (r.getId().equals("rectprof")) {
-                DespachoProfesores2.setOpacity(1);
-            } else {
-
-            }
-            r.setOpacity(1);
-        }
-
     }
 }
