6fc466f308296e5d856973735645229cf64b2fed
Bertitox <albercg18@gmail.com>
2025-04-10 12:15:43 +0200

    Nuevos iconos de errores, vistaPanel actualizada

--- src/main/java/org/example/adcontrol/ControladorPanelAula.java
+++ src/main/java/org/example/adcontrol/ControladorPanelAula.java
@@ -1,28 +1,4 @@
 package org.example.adcontrol;
 
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.Pane;
-
-public class ControladorPanelAula {
-    @FXML
-    private Pane panelGrande;
-
-    @FXML
-    void initialize() {
-        Platform.runLater(() -> {
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/panelAula.fxml"));
-                Parent root = fxmlLoader.load();
-                panelGrande.getChildren().clear();
-                panelGrande.getChildren().add(root);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
+public class ControladorPanelAula extends Controlador{
 }
---
+++ src/main/java/org/example/adcontrol/ControladorVistaPanelAula.java
@@ -0,0 +1,26 @@
+package org.example.adcontrol;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+public class ControladorVistaPanelAula extends Controlador{
+    @FXML
+    private Pane panelGrande;
+
+    @FXML
+    void initialize() {
+        Platform.runLater(() -> {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Vistas/panelAula.fxml"));
+                Parent root = fxmlLoader.load();
+                panelGrande.getChildren().clear();
+                panelGrande.getChildren().add(root);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
